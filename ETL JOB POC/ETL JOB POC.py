9c25aa2eed682818f6ca0d68fba7275b1065aaba
Edlaine Zamora <edlainetsz1@hotmail.com>
2024-01-04 12:25:22 -0300

    Synchronizing job ETL JOB POC from AWS Glue at 2024/01/04 15:25:22

--- ETL JOB POC/ETL JOB POC.py
+++ ETL JOB POC/ETL JOB POC.py
@@ -4,7 +4,6 @@ import logging
 import boto3
 import pyspark
 
-###################GLUE import##############
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.transforms import Relationalize
@@ -12,38 +11,33 @@ from awsglue.utils import getResolvedOptions
 from awsglue.job import Job
 from awsglue.transforms import *
 
-#### ###creating spark and gluecontext ###############
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'COLLECTION_NAME', 'DATABASE_NAME', 'OUTPUT_PREFIX', 'REGION_NAME', 'SECRET_NAME', 'PREFIX', 'BUCKET_NAME'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'BUCKET_NAME', 'COLLECTION_NAME', 'DATABASE_NAME', 'OUTPUT_PREFIX', 'OUTPUT_FILENAME', 'PREFIX', 'REGION_NAME', 'SECRET_NAME'])
 
 sc = pyspark.SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 
+logger = glueContext.get_logger()
+logger.info("Starting job")
+
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-## setup the MongoDB Credentials : update the Secret Name and Region ###
-def get_secret():
-
+def get_secret(logger):
+    logger.info("Setting up the MongoDB Credentials")
     secret_name = args['SECRET_NAME']
     region_name = args['REGION_NAME']
 
-    # Create a Secrets Manager client
+    logger.info("Creating Secrets Manager client")
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
-
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
 
     try:
         get_secret_value_response = client.get_secret_value(
@@ -52,8 +46,7 @@ def get_secret():
     except ClientError as e:
         raise e
     else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        logger.info("Decrypts secret using the associated KMS key")
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
             secrets_json = json.loads(secret)
@@ -62,9 +55,9 @@ def get_secret():
             decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
             return decoded_binary_secret
 
-user_name, password, server_addr = get_secret()
+user_name, password, server_addr = get_secret(logger)
 
-#### MongoDB Atlas Connection ###. 
+logger.info("Creating Atlas MongoDB connection")
 
 uri = "mongodb+srv://{}.mongodb.net/?retryWrites=true&w=majority&authSource=%24external&authMechanism=MONGODB-AWS".format(server_addr) 
 
@@ -79,18 +72,49 @@ read_mongo_options = {
     "password": password
 }
 
-
-## For Glue version 3.0 use "uri":uri, instead of "connection.uri":uri
-
-## Read from the MongoDB Atlas
+logger.info('Reading data from Atlas MongoDB')
 ds = glueContext.create_dynamic_frame_from_options(connection_type="mongodb", connection_options= read_mongo_options)
 
-logger = glueContext.get_logger()
-logger.info("Connecting...")
-
-# Write DynamicFrame to  s3 bucket
+logger.info('Writing DynamicFrame to s3 bucket in json format')
 path = "s3://{}/{}".format(args['BUCKET_NAME'], args['PREFIX'])
 glueContext.write_dynamic_frame.from_options(ds, connection_type = "s3", connection_options={"path": path}, format="json")
 
-logger.info("written to S3!")
-logger.info("Versioned!")
+logger.info('Writing DynamicFrame to s3 bucket in parquet format')
+path = "s3://{}/{}".format(args['BUCKET_NAME'], args['PREFIX'])
+glueContext.write_dynamic_frame.from_options(ds, connection_type = "s3", connection_options={"path": path}, format="parquet")
+
+logger.info('Renaming files on S3')
+import boto3
+
+client = boto3.client('s3')
+s3 = boto3.resource('s3')
+
+bucket = s3.Bucket(args['BUCKET_NAME'])
+for obj in bucket.objects.all():
+    logger.info(obj.key)
+    copy_source = {
+        'Bucket': args['BUCKET_NAME'], 
+        'Key': obj.key
+    }
+    if obj.key.startswith('{}/{}'.format(args['PREFIX'], 'run-')):
+        client.copy_object(
+            Bucket=args['BUCKET_NAME'],
+            CopySource=copy_source,
+            Key='{}/{}.json'.format(args['PREFIX'], args['OUTPUT_FILENAME']),
+        )
+        response = client.delete_object(
+            Bucket=args['BUCKET_NAME'],
+            Key=obj.key,
+        )
+    if obj.key.startswith('{}/{}'.format(args['PREFIX'], 'part-')):
+        client.copy_object(
+            Bucket=args['BUCKET_NAME'],
+            CopySource=copy_source,
+            Key='{}/{}.parquet'.format(args['PREFIX'], args['OUTPUT_FILENAME']),
+        )
+        response = client.delete_object(
+            Bucket=args['BUCKET_NAME'],
+            Key=obj.key,
+        )
+
+logger.info("Finished processing!")
